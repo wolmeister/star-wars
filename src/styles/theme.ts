f1803f131a9c46d2251e7219c91d2d56f15398b6
Victor Wolmeister <wolmeistervictor@gmail.com>
2019-12-20 22:53:25 -0300

    Create react project

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    background: '#000000',
+    primary: '#ffff00',
+    text: '#ffffff',
+  },
+};
+
+export default theme;
