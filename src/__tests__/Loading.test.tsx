c2936317c66f347aadda0c9de66114fb1ea89b8f
Victor Wolmeister <wolmeistervictor@gmail.com>
2019-12-24 03:09:46 -0300

    Add Loading test

---
+++ src/__tests__/Loading.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+
+import Loading from '~/components/Loading';
+import theme from '~/styles/theme';
+
+describe('Loading', () => {
+  test('renders correctly', () => {
+    const loading = render(
+      <ThemeProvider theme={theme}>
+        <Loading />
+      </ThemeProvider>
+    );
+    expect(loading).toMatchSnapshot();
+  });
+});
